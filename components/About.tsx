35afc906a86131e3e9277913b1d2c5077ad1c58c
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2025-03-14 12:56:52 +0300

    update about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,14 +1,17 @@
+import { Badge } from "@/components/ui/badge";
+
 export function About() {
   return (
     <div className="mt-16">
       <h1 className="font-bold">About me</h1>
       <div className="mt-3">
-        <span>
+        <span className="space-y-1">
           Hi, I&apos;m{" "}
-          <span className="important">ANDRIANALISOA Joséphin Sylvère</span> from{" "}
-          <span className="important">Madagascar</span> 🇲🇬 , a passionate
-          <span className="important"> backend developer</span> specializing in
-          <span className="important"> Node.js</span>. I enjoy building
+          <Badge className="important">ANDRIANALISOA Joséphin Sylvère</Badge>{" "}
+          from <Badge className="important">Madagascar 🇲🇬 </Badge>, a passionate
+          <Badge className="important"> backend developer</Badge> specializing
+          in
+          <Badge className="important"> Node.js</Badge>. I enjoy building
           efficient,scalable systems and optimizing performance to deliver
           hight-quality solutions.
         </span>
