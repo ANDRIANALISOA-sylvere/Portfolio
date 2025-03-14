bdb157daeb8a2dc364c62d9c7146d243283172b0
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2025-03-14 17:20:59 +0300

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import { App } from "@/components/App";
 
 export default function Home() {
   return (
-    <div className="flex h-screen">
-      <div className="w-1/4"></div>
-      <div className="w-1/2">
+    <div className="md:flex md:h-screen">
+      <div className="md:w-1/4"></div>
+      <div className="md:w-1/2">
         <div className="p-6">
           <App></App>
         </div>
       </div>
-      <div className="w-1/4"></div>
+      <div className="md:w-1/4"></div>
     </div>
   );
 }
