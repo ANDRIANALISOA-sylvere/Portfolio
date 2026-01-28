6d58ac6169dc8f12f5daa716a99e649c54c9f545
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2026-01-28 18:06:55 +0300

    Intern

--- src/components/common/Home.jsx
+++ src/components/common/Home.jsx
@@ -1,10 +1,12 @@
 import { HeroSection } from './HeroSection';
+import { Xp } from './Xp';
 
 export const Home = () => {
   return (
     <div className="min-h-screen flex flex-col items-center p-8">
       <div className="max-w-3xl w-full">
         <HeroSection />
+        <Xp></Xp>
       </div>
     </div>
   );
