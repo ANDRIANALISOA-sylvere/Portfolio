8d4f190d086a4797c7b78d1e7319b32b6c637034
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2026-04-01 00:10:13 +0300

    add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router";
 import { ThemeProvider } from './components/common/theme-provider';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <App />
+        <Analytics />
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
